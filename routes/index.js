1eced6d80a63006f91c314d63dbda333584e26c1
Valerio De Filippo <valerio.defilippo@gmail.com>
2022-02-11 17:14:52 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,41 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+//middleware
+function loginCheck(){
+  return (req, res, next) => {
+    if(req.session.user){
+      next()
+    } else {
+      res.redirect('/login')
+    }
+  }
+}
+
+const isLoggedIn = () => {
+  return (req, res, next) => {
+    if (req.session.user) {
+      next();
+    } else {
+      res.redirect('/login');
+    }
+  };
+};
+
+router.get("/profile", loginCheck(), (req, res, next) => {
+  const user = req.session.user
+  res.render("profile",{user: user});
+});
+
+
+router.get('/main',isLoggedIn(),  (req, res, next) => {
+  res.render('main')
+})
+
+router.get('/private',isLoggedIn(), (req, res, next) => {
+  res.render('private')
+})
+
+
+
 module.exports = router;
